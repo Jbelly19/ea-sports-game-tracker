b88af57505773957868f5c8074bade1a7e6d1cb7
Josh Belmont <jbelmont19@live.com>
2020-03-29 13:24:55 -0700

    Initial commit

---
+++ aaron/README.md
@@ -0,0 +1,3 @@
+# Image Recognition
+
+This folder can be used to store all the ML/Image recognition code. Feel free to do what ever you need with names/structure
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,50 +1,22 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 const Home = () => (
   <div className="container">
     <Head>
-      <title>Create Next App</title>
+      <title>EA Sports Game Tracker</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-
+      <h1 className="title">Welcome to EA Sports Game Tracker!</h1>
       <p className="description">
-        Get started by editing <code>pages/index.js</code>
+        This is a work in progress. This is a placeholder for the EA Sports Game
+        Tracker App
       </p>
-
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
-      </div>
+      <p className="body">
+        This page was built with React, Typescript, NextJS and hosted on ZEIT
+        Now.{' '}
+      </p>
     </main>
 
     <footer>
@@ -198,6 +170,6 @@ const Home = () => (
       }
     `}</style>
   </div>
-)
+);
 
-export default Home
+export default Home;
