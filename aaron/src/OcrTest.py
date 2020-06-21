d7f6b6053fa350ff82d57e098288b7aec80f7aae
Aaron <aapl365@utexas.edu>
2020-06-21 12:40:25 -0700

    Adding OCR pre-preprocessing examples and tests

--- aaron/src/OcrTest.py
+++ aaron/src/OcrTest.py
@@ -2,8 +2,10 @@
 Test script for image to text conversions (optical character recognition) (OCR)
 """
 
+import os
 import unittest
 
+import cv2
 import pytesseract
 
 
@@ -40,8 +42,50 @@ class OcrTest(unittest.TestCase):
             result: true if the converted text matches the expected test. false otherwise
             msg: failure string on failure. None otherwise
         """
-        converted_text = pytesseract.image_to_string(OcrTest.IMAGE_DIR + filename)
+        # tesseract --help-psm output:
+        #
+        # Page segmentation modes:
+        #   0    Orientation and script detection (OSD) only.
+        #   1    Automatic page segmentation with OSD.
+        #   2    Automatic page segmentation, but no OSD, or OCR. (not implemented)
+        #   3    Fully automatic page segmentation, but no OSD. (Default)
+        #   4    Assume a single column of text of variable sizes.
+        #   5    Assume a single uniform block of vertically aligned text.
+        #   6    Assume a single uniform block of text.
+        #   7    Treat the image as a single text line.
+        #   8    Treat the image as a single word.
+        #   9    Treat the image as a single word in a circle.
+        #  10    Treat the image as a single character.
+        #  11    Sparse text. Find as much text as possible in no particular order.
+        #  12    Sparse text with OSD.
+        #  13    Raw line. Treat the image as a single text line,
+        #        bypassing hacks that are Tesseract-specific.
+        pre_process_filename = OcrTest._pre_process_image(OcrTest.IMAGE_DIR + filename)
+        tesseract_args = '--oem 1 --psm 11'
+        converted_text = pytesseract.image_to_string(pre_process_filename, lang='eng+osd', config=tesseract_args)
         return (expected_text == converted_text), 'actual: {}, expected: {}'.format(converted_text, expected_text)
+
+    @staticmethod
+    def _pre_process_image(filename):
+        # Gaussian Blur 5,5 kernel
+        # Best 200, 255 thresh
+
+        # Best 190, 255 thresh
+        (dir, basename) = os.path.split(filename)
+
+        # Write grayscale
+        img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+        cv2.imwrite('{}/gray/{}'.format(dir, basename), img)
+
+        # Gaussian blur
+        #img = cv2.GaussianBlur(img, (5, 5), 0)
+
+        # Write threshold
+        unused, img = cv2.threshold(img, 190, 255, cv2.THRESH_BINARY_INV)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        cv2.imwrite('{}/pre/{}'.format(dir, basename), img)
+
+        return '{}/pre/{}'.format(dir, basename)
 
     def test_score(self):
         """
@@ -70,6 +114,15 @@ class OcrTest(unittest.TestCase):
         """
         (result, msg) = OcrTest._image_to_text_test('powerplay_right_20200329_020409468_iOS.jpg', '0/0')
         self.assertTrue(result, msg=msg)
+
+    def test_page(self):
+        """
+        Test parsing a whole screenshot without cropping
+        :return:
+        """
+        (result, msg) = OcrTest._image_to_text_test('all_left_20200329_020409468_iOS.jpg', 'tester')
+        self.assertTrue(result, msg=msg)
+
 
 
 if __name__ == '__main__':
